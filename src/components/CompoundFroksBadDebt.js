5f72565132c39600ef4f3faceb2f25805df63699
shmuel <shmuel@shmuels-MacBook-Pro.local>
2022-06-06 12:42:48 +0300

    changes

--- src/components/CompoundFroksBadDebt.js
+++ src/components/CompoundFroksBadDebt.js
@@ -12,14 +12,25 @@ class CompoundFroksBadDebt extends Component {
   }
 
   render () {
+    const theme = mainStore.blackMode ? 'dark' : 'light';
     return (
       <div>
         {mainStore.loading && <div>
           <article style={{minHeight: '80vh'}} aria-busy="true"></article>
         </div>}
         {!mainStore.loading &&  <article>
-          <h2>Compound Forks Bad Debt</h2>
+          <header>
+            <h2 style={{margin: 0, textAlign: 'center'}}>Compound Compatible Platform Bad Debt</h2>
+          </header>
           <TableView data={mainStore.tableData}/>
+          <footer style={{display: 'flex', justifyContent: 'space-around'}}>
+            <a href="https://krystal.app" target="_blank">
+              <img style={{ width:"254px", height:"54px" }} src={`/images/power-krystal-${theme}.svg`}/>
+            </a>
+            <a href="https://zapper.fi" target="_blank">
+              <img src={`/images/power-zapper-${theme}.svg`}/>
+            </a>
+          </footer>
         </article>}
       </div>
     )
