51c2cee516a4af4536910db11bf6aaf831a2df68
shmuel <shmuel@shmuels-MacBook-Pro.local>
2022-06-16 10:45:46 +0300

    invesre finance comnts

--- src/lending-platfroms-details/index.js
+++ src/lending-platfroms-details/index.js
@@ -1,11 +1,13 @@
 import IronBank from './IronBank'
+import Inverse from './Inverse'
 
 /**
  * use kabab-case to register your platform component
  * components should be a simple function rendering simple HTML
  */
 const register = {
-  "iron-bank": IronBank
+  "iron-bank": IronBank,
+  "inverse": Inverse
 }
 
 export default register
