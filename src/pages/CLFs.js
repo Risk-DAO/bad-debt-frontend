e09d78a8b5c9cd0e26d9b0dd33283bc4f53ea34f
Edouard VIGUIER <33686588+0xVorian@users.noreply.github.com>
2023-08-12 21:45:45 +0200

    CLF page

---
+++ src/pages/CLFs.js
@@ -0,0 +1,10 @@
+function CLFs(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const platform = urlParams.get('platform')
+
+    return(
+        <div>
+            Work In Progress
+        </div>
+    )
+}
