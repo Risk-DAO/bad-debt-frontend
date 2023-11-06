cbad1e139dcaa4641edd6b72d9674e8d2b7bdfa1
Edouard VIGUIER <33686588+0xVorian@users.noreply.github.com>
2023-11-06 16:38:49 +0100

    test

--- src/components/CLFProtocolAverage.js
+++ src/components/CLFProtocolAverage.js
@@ -2,10 +2,24 @@ import { Divider, Tooltip as MUITooltip } from "@mui/material";
 import { MathComponent } from "mathjax-react";
 import InfoIcon from '@mui/icons-material/Info';
 import { useEffect, useState } from "react";
-import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { CartesianGrid, Label, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 import { CLFNumberFormatter } from "../utils";
 import CircularProgress from '@mui/material/CircularProgress';
 import moment from "moment";
+
+function RLegend() {
+    return (
+        <div>
+            <MUITooltip title="Weighted average across all markets">
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    r
+                    <Divider orientation="vertical" />
+                    <InfoIcon fontSize="small" />
+                </div>
+            </MUITooltip>
+        </div>
+    )
+}
 
 export default function CLFProtocolAverage(props) {
     const protocol = props.protocol;
@@ -14,27 +28,30 @@ export default function CLFProtocolAverage(props) {
 
 
     useEffect(() => {
-            if(averageData && averageData['protocolAverageHistory']){
-                const data = [];
-                for(const [timestamp, value] of Object.entries(averageData['protocolAverageHistory'])){
-                    data.push({'date': timestamp,
-                'weightedAverage': value})
-                }
-                data.sort((a,b)=> a.date - b.date);
-                setGraphData(data);
-    }}, [averageData]);
+        if (averageData && averageData['protocolAverageHistory']) {
+            const data = [];
+            for (const [timestamp, value] of Object.entries(averageData['protocolAverageHistory'])) {
+                data.push({
+                    'date': timestamp,
+                    'weightedAverage': value
+                })
+            }
+            data.sort((a, b) => a.date - b.date);
+            setGraphData(data);
+        }
+    }, [averageData]);
 
-    function xAxisFormat(timestamp){
+    function xAxisFormat(timestamp) {
         const date = moment(Number(timestamp));
-  const formattedDate = moment(date, "DD.MM.YYYY").format('MMM DD');
+        const formattedDate = moment(date, "DD.MM.YYYY").format('MMM DD');
         return formattedDate
     }
 
     function RiskLevelNumberFormatter(number) {
         return [number.toFixed(2), 'Risk Level'];
-      }
+    }
 
-      console.log(averageData)
+    console.log(averageData)
 
     return (
         <div className="CLFMarket">
@@ -50,39 +67,42 @@ export default function CLFProtocolAverage(props) {
                         </article>
                         <Divider orientation="vertical" />
                         <article className="CLFPoolNameTab">
-                            <b><em>r</em> = {graphData.slice(-1)[0]['weightedAverage']}</b>
+                            <b><em>r</em> = {graphData ? graphData.slice(-1)[0]['weightedAverage'] : ''}</b>
                         </article>
                     </div>
                     <article className="CLFProtocolAverage" style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start' }}>
                         <div className="RiskFormula">
-                            <MUITooltip title="Weighted average across all markets">
-                                <div style={{ display: "flex", flexDirection: "row" }}>
-                                    <h6>Risk Level Index </h6>
-                                    <Divider orientation="vertical" />
-                                    <InfoIcon fontSize="small" />
-                                </div>
-                            </MUITooltip>
                             <MathComponent tex={String.raw`r = \frac{\sigma \cdot \sqrt{d}}{\ln\frac{1}{(LTV + \beta)}\cdot\sqrt{l}}`} />
                         </div>
                         <div className="CLFProtocolAverageGraph">
                             <ResponsiveContainer width="100%" height="100%" minHeight="350px">
                                 {graphData ?
                                     <LineChart
-                                    data={graphData}
-                                    margin={{
-                                        top: 25,
-                                        right: 0,
-                                        left: 10,
-                                        bottom: 60,
-                                    }}
-                                >
-                                    <CartesianGrid vertical={false} horiz strokeDasharray="3 3" />
-                                    <XAxis dataKey="date" minTickGap={10} tickFormatter={xAxisFormat} />
-                                    <YAxis type="number" label={{ value: 'r', offset: '45', position: 'top' }} tickMargin={5} tickFormatter={CLFNumberFormatter} />
-                                    <Tooltip formatter={RiskLevelNumberFormatter} labelFormatter={xAxisFormat}
-                                        wrapperClassName="card shadow" />
-                                    <Line dot={false} key='weightedAverage' type="monotone" stroke={'#3182BD'} dataKey='weightedAverage' activeDot={{ r: 8 }} />
-                                </LineChart> : <CircularProgress />}
+                                        data={graphData}
+                                        margin={{
+                                            top: 55,
+                                            right: 0,
+                                            left: 10,
+                                            bottom: 60,
+                                        }}
+                                    >
+                                        <CartesianGrid vertical={false} horiz strokeDasharray="3 3" />
+                                        <XAxis dataKey="date" minTickGap={10} tickFormatter={xAxisFormat} />
+                                        <YAxis type="number" tickMargin={5} tickFormatter={CLFNumberFormatter}>
+                                            <Label offset={30} position='top'>
+                                                <MUITooltip title="Weighted average across all markets">
+                                                    <div style={{ display: "flex", flexDirection: "row" }}>
+                                                        r
+                                                        <Divider orientation="vertical" />
+                                                        <InfoIcon fontSize="small" />
+                                                    </div>
+                                                </MUITooltip>
+                                            </Label>
+                                        </YAxis>
+                                        <Tooltip formatter={RiskLevelNumberFormatter} labelFormatter={xAxisFormat}
+                                            wrapperClassName="card shadow" />
+                                        <Line dot={false} key='weightedAverage' type="monotone" stroke={'#3182BD'} dataKey='weightedAverage' activeDot={{ r: 8 }} />
+                                    </LineChart> : <CircularProgress />}
                             </ResponsiveContainer>
                         </div>
                     </article>
