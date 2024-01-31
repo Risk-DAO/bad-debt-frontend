badc69566193ceb58595dd9ce2d883eaff424b69
0xVorian <33686588+0xVorian@users.noreply.github.com>
2024-01-31 18:07:29 +0100

    Update MIM.js

--- src/lending-platfroms-details/MIM.js
+++ src/lending-platfroms-details/MIM.js
@@ -3,7 +3,7 @@ const MIM = ()=> {
       <div>
         <h5>MIM - January 2024 hack</h5>
         <blockquote>
-        Because of the January 2024 hack the debt for the yv-3Crypto and the magicAPE cauldrons are calculated as follow: outstanding cauldron borrow - TVL.
+        Following <a href="https://twitter.com/EXVULSEC/status/1752357798783103158" rel="noopener noreferrer">the January 2024 hack</a> the debt for the yv-3Crypto and the magicAPE cauldrons are calculated as follow: outstanding cauldron borrow - TVL.
         <br/>
         <br/>
           <footer>
